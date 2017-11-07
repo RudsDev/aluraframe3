4b56d971f702f7d99041f4dd1ecadbee2a4d2904
Rafael Dias <rafael@hostnet.com.br>
2017-11-07 10:28:11 -0200

    Aula_2 [#5 - Monkey Patch: grandes poderes trazem grandes responsabilidades].

--- client/ann/Parte_3/Aula - 2/Aula_5.js
+++ client/ann/Parte_3/Aula - 2/Aula_5.js
@@ -1,5 +1,95 @@
 /* Aula_5 */
 
 
+/*/## 5 - Monkey Patch: grandes poderes trazem grandes responsabilidades ##/*/
 
-/*/## 5 -  ##/*/
+
+/* Para finalizar a classe ConnectionFactory falta atender um último
+requisito: 
+
+D) Toda conexão possui o método close, mas o programador não
+pode chamá-lo, porque a conexão é a mesma para a aplicação inteira.
+
+
+Para tal, será usado um "Monkey patch", ou seja, a API será alterada
+a força.
+
+
+*/
+
+
+var ConnectionFactory = (function tempConnectionFactory() {
+    
+    const stores = ['negociacoes'];
+    const version = 3;
+    const dbName = 'aluraframe';
+    
+    var connection = null;
+    var close = null;
+    
+
+    return class ConnectionFactory{
+    
+        constructor(){
+            throw new Error('Não é possível criar instâncias dessa classe.');
+        }
+    
+        static getConnection(){
+    
+            return new Promise((resolve, reject)=>{
+    
+                let openRequest = window.indexedDB.open(stores,version);
+    
+                openRequest.onupgradeneeded = (event)=>{
+                    ConnectionFactory._createStore(event.target.result);
+                };
+    
+                openRequest.onsuccess = (event)=>{
+    
+                    if(!connection){
+                        connection = event.target.result;
+                        /*Antes de sobrescrever o método close da API
+                        atribuimos o mesmo a variavel close.
+                        Isso é feito para que seja possivel fechar
+                        uma conexão mesmo após sobrescrever o método
+                        close da API.*/
+                        close = connection.close.bind(connection);
+                        
+                        //Monkey patch
+                        connection.close = ()=>{
+                            throw new Error('Essa conexão não pode ser fechada diretamente.');
+                        }
+                    }
+                    resolve(connection);
+                };
+    
+                openRequest.onerror = (event)=>{
+                    console.log(event.target.error);
+                    reject(event.target.error.name);
+                };
+            });
+        }
+
+
+        /*Função que será responsável por fechar a conexão.
+            */
+        static closeConnection(){
+            if(connection){
+                //Fecha a conexão pela API.
+                close();
+
+                /*Atribui null a connection para que
+                uma nova conexão seja criada.*/
+                connection = null;
+            }
+        }
+    
+        static _createStore(conn){
+            stores.forEach((store)=>{
+                if(conn.objectStoreNames.contains(store))
+                    conn.deleteObjectStore(store);
+                conn.createObjectStore(store,{autoincrement:true})
+            });
+        }
+    }    
+})();
--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,46 +1,60 @@
 var ConnectionFactory = (function tempConnectionFactory() {
     
-        var stores = ['negociacoes'];
-        var version = 3;
-        var dbName = 'aluraframe';
-        var connection = null;
-        
-        return class ConnectionFactory{
-        
-            constructor(){
-                throw new Error('Não é possível criar instâncias dessa classe.');
+    const stores = ['negociacoes'];
+    const version = 3;
+    const dbName = 'aluraframe';
+    
+    var connection = null;
+    var close = null;
+    
+    return class ConnectionFactory{
+    
+        constructor(){
+            throw new Error('Não é possível criar instâncias dessa classe.');
+        }
+    
+        static getConnection(){
+    
+            return new Promise((resolve, reject)=>{
+    
+                let openRequest = window.indexedDB.open(stores,version);
+    
+                openRequest.onupgradeneeded = (event)=>{
+                    ConnectionFactory._createStore(event.target.result);
+                };
+    
+                openRequest.onsuccess = (event)=>{
+    
+                    if(!connection){
+                        connection = event.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = ()=>{
+                            throw new Error('Essa conexão não pode ser fechada diretamente.');
+                        }
+                    }
+                    resolve(connection);
+                };
+    
+                openRequest.onerror = (event)=>{
+                    console.log(event.target.error);
+                    reject(event.target.error.name);
+                };
+            });
+        }
+
+        static closeConnection(){
+            if(connection){
+                close();
+                connection = null;
             }
-        
-            static getConnection(){
-        
-                return new Promise((resolve, reject)=>{
-        
-                    let openRequest = window.indexedDB.open(stores,version);
-        
-                    openRequest.onupgradeneeded = (event)=>{
-                        ConnectionFactory._createStore(event.target.result);
-                    };
-        
-                    openRequest.onsuccess = (event)=>{
-        
-                        if(!connection)
-                            connection = event.target.result;
-                        resolve(connection);
-                    };
-        
-                    openRequest.onerror = (event)=>{
-                        console.log(event.target.error);
-                        reject(event.target.error.name);
-                    };
-                });
-            }
-        
-            static _createStore(conn){
-                stores.forEach((store)=>{
-                    if(conn.objectStoreNames.contains(store))
-                        conn.deleteObjectStore(store);
-                    conn.createObjectStore(store,{autoincrement:true})
-                });
-            }
-        }    
-    })();
+        }
+    
+        static _createStore(conn){
+            stores.forEach((store)=>{
+                if(conn.objectStoreNames.contains(store))
+                    conn.deleteObjectStore(store);
+                conn.createObjectStore(store,{autoincrement:true})
+            });
+        }
+    }    
+})();
