0e58b34b260df095905d11260988b7de4821bf98
Rafael Dias <rafael@hostnet.com.br>
2017-11-07 08:54:12 -0200

    Aula_2 [#2 - A classe ConnectionFactory].

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,27 +1,6 @@
-/* Aula_2 */
-
-
-
-/*/## 2 - A classe ConnectionFactory ##/*/
-
-
-/*A classe ConnectionFactory deve ser o
-mais generérica possivel para que se possa utiliza-la
-para criar outros bancos/conexões.
-Para tal serão utilizadas variaveis que receberão
-os dados dos bancos/conexões a serem criados.
-Contudo, devido ao fato de ConnectionFactory ser uma 
-classe que não permite a criação de instâncias, 
-não é possivel declarar os atributos dentro do 
-construtor da classe, como é de costume./*
-
-/*
-Temporariamente será feito essa gamby tenebrosa
-apenas para fins didáticos.  */
 var stores = ['negociacoes'];
 var version = 3;
 var dbName = 'aluraframe';
-
 
 class ConnectionFactory{
 
@@ -46,9 +25,6 @@ class ConnectionFactory{
             openRequest.onerror ((event)=>{
                 
             })
-
         });
-
     }
-
 }
