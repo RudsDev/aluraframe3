426fdb78fc5a134c61724ec65cc5d4fa0fba92d6
Rafael Dias <rafael@hostnet.com.br>
2017-11-09 13:51:58 -0200

    Aula_4 ['#4 - Importando negociações automaticamente'].

--- client/ann/Parte_3/Aula - 4/Aula-4.js
+++ client/ann/Parte_3/Aula - 4/Aula-4.js
@@ -2,4 +2,13 @@
 
 
 
-/*/## 4 -  ##/*/
+/*/## 4 - Importando negociações automaticamente ##/*/
+
+
+/*
+
+Importando as negociações automaticamentes com o uso
+da função setInterval e retirando responsabilidades 
+do construtor de NegociacaoController.
+
+*/
--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,8 +15,82 @@ class NegociacaoController {
       
         this._mensagem = new Bind(new Mensagem(),
                                   new MensagemView($('#mensagemView')),
-                                  'texto');
+                                  'texto');       
+        this._init(); 
 
+        Object.freeze(this);
+
+        }
+                                
+    adiciona(event){
+        
+        event.preventDefault();
+        
+        ConnectionFactory.getConnection()
+        .then((connection)=>{
+            
+            let negociacao = this._criarNegociacao();
+
+            new NegociacaoDao(connection)
+            .adiciona(negociacao)
+            .then(()=>{
+                this._mensagem.texto = 'Negociação adicionada com sucesso';
+                    this._listagem.adiciona(negociacao);
+                    this._limpaFormulario();
+                })
+                .catch((erro)=>{
+                    this._mensagem.texto = erro;
+                });
+            });     
+    }
+    
+    esvaziarLista(){
+        
+        ConnectionFactory.getConnection()
+        .then(connection => new NegociacaoDao(connection))
+        .then(dao => dao.apagaTodas())
+        .then(mensagem =>{
+            this._mensagem.texto = mensagem;
+            this._listagem.esvaziarLista();
+        });
+    }
+    
+    importarNegociacoes() {
+        
+        
+        let service = new NegociacaoService();
+        service
+        .obterNegociacoes()
+        .then(negociacoes =>
+            negociacoes.filter(negociacao =>
+    
+                this._listagem.negociacoes.some(negociacaoInList =>
+                    JSON.stringify(negociacao)==JSON.stringify(negociacaoInList)
+                ))
+            )
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listagem.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas';   
+            }))
+            .catch(erro => this._mensagem.texto = erro);                              
+    }
+    
+    _criarNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
+        }
+        
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 0;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+    
+    _init(){
+        
         ConnectionFactory
             .getConnection()
             .then(connection => new NegociacaoDao(connection))
@@ -28,74 +102,10 @@ class NegociacaoController {
                 console.log(erro);
                 this._mensagem.texto = erro;
             })
-
-        Object.freeze(this);
-    }
-
-    adiciona(event){
-
-        event.preventDefault();
-
-        ConnectionFactory.getConnection()
-        .then((connection)=>{
-
-            let negociacao = this._criarNegociacao();
-
-            new NegociacaoDao(connection)
-                .adiciona(negociacao)
-                .then(()=>{
-                    this._mensagem.texto = 'Negociação adicionada com sucesso';
-                    this._listagem.adiciona(negociacao);
-                    this._limpaFormulario();
-                })
-                .catch((erro)=>{
-                    this._mensagem.texto = erro;
-                });
-        });     
-    }
-
-    esvaziarLista(){
-
-        ConnectionFactory.getConnection()
-        .then(connection => new NegociacaoDao(connection))
-        .then(dao => dao.apagaTodas())
-        .then(mensagem =>{
-            this._mensagem.texto = mensagem;
-            this._listagem.esvaziarLista();
-        });
-    }
-
-    importarNegociacoes() {
         
-    
-        let service = new NegociacaoService();
-        service
-            .obterNegociacoes()
-            .then(negociacoes =>
-                negociacoes.filter(negociacao =>
-    
-                    this._listagem.negociacoes.some(negociacaoInList =>
-                    JSON.stringify(negociacao)==JSON.stringify(negociacaoInList)
-                ))
-            )
-            .then(negociacoes => negociacoes.forEach(negociacao => {
-                this._listagem.adiciona(negociacao);
-                this._mensagem.texto = 'Negociações do período importadas';   
-            }))
-            .catch(erro => this._mensagem.texto = erro);                              
-    }
-
-    _criarNegociacao(){
-        return new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
-    }
-
-    _limpaFormulario(){
-        this._inputData.value = '';
-        this._inputQuantidade.value = 0;
-        this._inputValor.value = 0.0;
-        this._inputData.focus();
+        setInterval(()=>{
+            console.log('Importando Negociações');
+            this.importarNegociacoes();
+        },3000);
     }
 }
