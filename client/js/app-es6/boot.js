344ac2f749c8a3176e8d83172d8514e5fdfaf41f
Rafael Dias <ruds.devel@gmail.com>
2017-11-11 12:08:07 -0200

    Aula_7 ['#5 - O módulo boot.js'].

---
+++ client/js/app-es6/boot.js
@@ -0,0 +1,11 @@
+import {NegociacaoController} from './controllers/NegociacaoController';
+import {} from '/polyfill/fetch';
+
+let negociacaoController = new NegociacaoController();
+
+/*Retirando a reponsabilidade de ligação dos btns coms os métodos
+apagar e adicionar da View e delegando para o boot.js*/
+document.querySelector('.form').onsubmit = 
+    negociacaoController.adiciona.bind(negociacaoController);
+document.querySelector('[type=button]').onclick = 
+    negociacaoController.apaga.bind(negociacaoController);
